17448ecb535fec280f2d9df0ebbdd758ec8f906c
Nasir Ayaz <knasirayaz@gmail.com>
2024-10-05 07:17:34 +0500

    removed-examples

--- example-viewbinding/src/main/java/com/xwray/groupie/example/viewbinding/HeaderItemDecoration.kt
+++
@@ -1,9 +0,0 @@
-package com.xwray.groupie.example.viewbinding
-
-import androidx.annotation.ColorInt
-import com.xwray.groupie.example.core.decoration.HeaderItemDecoration
-
-class HeaderItemDecoration(
-    @ColorInt background: Int,
-    sidePaddingPixels: Int
-) : HeaderItemDecoration(background, sidePaddingPixels, R.layout.item_header)
